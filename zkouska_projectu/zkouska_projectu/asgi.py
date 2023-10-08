318b5ffd54e4bd70f1118516ca980ad6f6b0f4cc
unknown <you@example.com>
2023-10-08 21:37:44 +0200

    Pridavam rovnou celi adresar mainapp

---
+++ zkouska_projectu/zkouska_projectu/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for zkouska_projectu project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zkouska_projectu.settings')
+
+application = get_asgi_application()
